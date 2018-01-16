0f657f5d9d7be0d69f65d5dd74337a84f396f793
Olga Il <olgail@wp.pl>
2018-01-16 22:08:46 +0100

    check if photon is connecting

--- Assets/Scripts/PhotonNetworkManager.cs
+++ Assets/Scripts/PhotonNetworkManager.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class PhotonNetworkManager : Photon.MonoBehaviour {
 
     [SerializeField]
-    private Text connectText; 
+    private Text connectText;
+
+    [SerializeField]
+    private Canvas canvas; 
 
 	// Use this for initialization
 	void Start () {
         PhotonNetwork.ConnectUsingSettings("0.1");
+        connectText.transform.parent = canvas.transform; 
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //for testing only!!!!!!!! Delete later!!!!!
+        connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
 	}
 }
